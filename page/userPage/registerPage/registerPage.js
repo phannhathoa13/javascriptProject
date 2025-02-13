20a1b9e7c071374e13f9d6b7d0f193e8e7e15cb6
hoafan <Phannhathoa13@gmail.com>
2025-02-13 18:10:08 +0700

    done register,

--- page/userPage/registerPage/registerPage.js
+++ page/userPage/registerPage/registerPage.js
@@ -5,7 +5,6 @@ import User from "../../../models/user.js";
 import { validationEmail, validationPassword, validationUsername } from "../../../validation/loginValidation.js";
 
 const listUser = await fetchUserAPI();
-
 const usernameWarning = document.getElementById('usernameWarning');
 const passwordWarning = document.getElementById('passwordWarning');
 const confirmPasswordWarning = document.getElementById('cfpwdWarning');
@@ -15,82 +14,132 @@ const usernameFather = document.getElementById('username');
 const passwordFather = document.getElementById('pwd');
 const confirmPasswordFather = document.getElementById('cfpwd');
 const emailFather = document.getElementById('email');
+const registerButton = document.getElementById('register');
+registerButton.disabled = true;
 
+let isUsernameValid = false;
+let isPasswordValid = false;
+let isConfirmPasswordValid = false;
+let isEmailValid = false;
 
-document.getElementById('form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const usernameInput = usernameFather.value;
-    const passwordInput = passwordFather.value;
-    const confirmPassword = confirmPasswordFather.value;
-    const emailInput = emailFather.value;
-    const formData = new FormData(this);
+function isUsernameExisted(usernameInput) {
+    return listUser.some((user) => user.username == usernameInput);
+}
 
+function isEmailExisted(emailInput) {
+    return listUser.some((user) => user.email == emailInput);
+}
 
+window.validateUsername = function validateUsername(event) {
+    const usernameInput = event.target.value;
     if (!usernameInput) {
-        usernameFather.style.border = "1px solid rgb(205, 82, 82)";
+        errorMassage(usernameFather, usernameWarning);
         usernameWarning.innerHTML = "Your username is empty !";
         return;
     }
     else if (!validationUsername(usernameInput)) {
-        usernameFather.style.border = "1px solid rgb(205, 82, 82)";
+        errorMassage(usernameFather, usernameWarning);
         usernameWarning.innerHTML = "Your username is not valid !";
         return;
     }
     else if (isUsernameExisted(usernameInput)) {
-        usernameFather.style.border = "1px solid rgb(205, 82, 82)";
-        usernameWarning.innerHTML = "Your username is existed !";
-        return;
-    }
-    else if (!passwordInput) {
-        passwordFather.style.border = "1px solid rgb(205, 82, 82)";
-        passwordWarning.innerHTML = "Your password is empty !";
-        return;
-    }
-    else if (!confirmPassword) {
-        confirmPasswordFather.style.border = "1px solid rgb(205, 82, 82)";
-        confirmPasswordWarning.innerHTML = "Your confirm password is empty !";
-        return;
-    }
-    else if (!validationPassword(passwordInput)) {
-        passwordFather.style.border = "1px solid rgb(205, 82, 82)";
-        if (passwordInput.length < 8) {
-            passwordWarning.innerHTML = "Your password must over 8 character";
-        }
-        else if (passwordInput.toLowerCase()) {
-            passwordWarning.innerHTML = "Your password must have 1 sentisive character";
-        }
-        else if (!passwordInput.includes("!@#$%^&")) {
-            passwordWarning.innerHTML = "Your password must have 1 special character";
-        }
-        return;
-    }
-    else if (passwordInput != confirmPassword) {
-        passwordFather.style.border = "1px solid rgb(205, 82, 82)";
-        confirmPasswordWarning.innerHTML = "Your password or confirm password is not the same !";
-        return;
-    }
-    else if (!emailInput) {
-        emailFather.style.border = "1px solid rgb(205, 82, 82)";
-        emailWarning.innerHTML = "Your email is empty !";
-        return;
-    }
-    else if (!validationEmail(emailInput)) {
-        emailFather.style.border = "1px solid rgb(205, 82, 82)";
-        if (!emailInput.includes("@")) {
-            emailWarning.innerHTML = "Your email must have @";
-        }
-        return;
-    }
-    else if (isEmailExisted(emailInput)) {
-        emailFather.style.border = "1px solid rgb(205, 82, 82)";
-        emailWarning.innerHTML = "Your email is existed";
+        errorMassage(usernameFather, usernameWarning);
+        usernameWarning.innerHTML = "Your username is existed !, please try new one";
         return;
     }
     else {
-        registerAccount(formData);
-        window.alert("register susceed");
-        window.location.href = "../loginPage/loginPage.html";
+        usernameWarning.innerHTML = "";
+        usernameFather.style.border = "1px solid black";
+        isUsernameValid = true;
+        updateButtonRegister();
+        return;
     }
+}
+
+window.validatePassword = function validatePassword(event) {
+    const passwordInput = event.target.value;
+    if (!passwordInput) {
+        errorMassage(passwordFather, passwordWarning);
+        passwordWarning.innerHTML = "Your password is empty !";
+        return;
+    }
+    else if (!validationPassword(passwordInput)) {
+        errorMassage(passwordFather, passwordWarning);
+        passwordWarning.innerHTML = "Your password must have 1 special character, 1 uppercase letter and must longer then 8 character";
+        return;
+    }
+    else {
+        passwordWarning.innerHTML = "";
+        passwordFather.style.border = "1px solid black";
+        isPasswordValid = true;
+        updateButtonRegister();
+        return;
+    }
+}
+
+window.validateConfirmPassword = function validateConfirmPassword(event) {
+    const confirmPasswordInput = event.target.value;
+    const passwordInputValue = passwordFather.value;
+    if (!confirmPasswordInput) {
+        errorMassage(confirmPasswordFather, confirmPasswordWarning);
+        confirmPasswordWarning.innerHTML = "Your confirm password is empty";
+        return;
+    }
+    else if (confirmPasswordInput != passwordInputValue) {
+        errorMassage(confirmPasswordFather, confirmPasswordWarning);
+        confirmPasswordWarning.innerHTML = "Your password and confirm password must the same";
+        return;
+    }
+    else {
+        confirmPasswordWarning.innerHTML = "";
+        confirmPasswordFather.style.border = "1px solid black";
+        isConfirmPasswordValid = true;
+        updateButtonRegister();
+        return
+    }
+}
+
+window.validateEmail = function validateEmail(event) {
+    const emailInput = event.target.value;
+    if (!emailInput) {
+        errorMassage(emailFather, emailWarning);
+        emailWarning.innerHTML = "Your email is empty";
+        return;
+    }
+    else if (!validationEmail(emailInput)) {
+        errorMassage(emailFather, emailWarning);
+        emailWarning.innerHTML = "Your email is not valid ";
+        return;
+    }
+    else if (isEmailExisted(emailInput)) {
+        errorMassage(emailFather, emailWarning);
+        emailWarning.innerHTML = "Your email is existed !, please try new one ";
+    }
+    else {
+        emailWarning.innerHTML = "";
+        emailFather.style.border = "1px solid black";
+        isEmailValid = true;
+        updateButtonRegister();
+        return;
+    }
+}
+
+function errorMassage(valueInput, textWarning) {
+    valueInput.style.border = "1px solid rgb(205, 82, 82)";
+    textWarning.style.color = "red";
+}
+
+function updateButtonRegister() {
+    if (isUsernameValid && isPasswordValid && isConfirmPasswordValid && isEmailValid) {
+        registerButton.disabled = false;
+    }
+}
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    registerAccount(formData);
+    window.alert("register susceed");
+    window.location.href = "../loginPage/loginPage.html";
 })
 
 function registerAccount(formData) {
@@ -108,13 +157,4 @@ function registerAccount(formData) {
     )
     registerAccountUser(user);
     registerCartUser(cart);
-}
-
-function isUsernameExisted(usernameInput) {
-    return listUser.some((user) => user.username == usernameInput);
-}
-
-function isEmailExisted(emailInput) {
-    return listUser.some((user) => user.email == emailInput);
-
 }
--- validation/loginValidation.js
+++ validation/loginValidation.js
@@ -1,5 +1,5 @@
 const validationUsername = (usernameInput) => {
-    return /^[a-zA-Z0-9_!@#$%^&;,./]+$/.test(usernameInput);
+    return /^[a-zA-Z0-9_!@#$%^&;,./]+.{4,}$/.test(usernameInput);
 }
 const validationPassword = (passwordInput) => {
     return /^(?=.*[a-zA-Z0-9])(?=.*[!@#$%^&]).{8,}$/.test(passwordInput);
